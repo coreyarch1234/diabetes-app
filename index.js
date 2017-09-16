87beb967d31761d6a04cb9bea86797b0766cf6b8
coreyarch1234 <harrilal@mit.edu>
2017-09-16 09:24:35 -0700

    started project

---
+++ index.js
@@ -0,0 +1,26 @@
+//Express
+var express = require('express');
+var exphbs  = require('express-handlebars');
+//App
+var app = express();
+//To parse post requests
+var bodyParser = require('body-parser');
+
+var port = 3000;
+
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Setting templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || port, function() {
+    console.log("app is running");
+    console.log("env port" + process.env.PORT);
+})
